3fa60d923e588689b51f3deef7b0669fff793209
Jelle Kuipers <info@jellekuipers.com>
2025-01-30 20:57:50 +0100

    Change routing

--- app/components/button.tsx
+++ app/components/button.tsx
@@ -9,7 +9,7 @@ export const Button = ({
 }) => {
   return (
     <button
-      className="rounded-md bg-black px-3 py-2 font-semibold text-white hover:bg-black/80"
+      className="rounded-md bg-black px-4 py-2 font-semibold text-white hover:bg-black/80"
       onClick={onClick}
       type={type}
     >
--- app/routes/auth.tsx
+++ app/routes/auth.tsx
@@ -1,7 +1,7 @@
-// This _auth component functions as a layout component that wraps all authentication routes in the app.
+// This auth component functions as a layout component that wraps all authentication routes in the app, prepending related endpoints with /auth
 import { createFileRoute, Outlet, redirect } from "@tanstack/react-router";
 
-export const Route = createFileRoute("/_auth")({
+export const Route = createFileRoute("/auth")({
   beforeLoad: ({ context: { session } }) => {
     if (session?.user) {
       throw redirect({
