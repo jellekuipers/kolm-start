59cc9bea9123a31598f37b5fc4987accd6158be8
Jelle Kuipers <info@jellekuipers.com>
2025-03-06 09:04:43 +0100

    Update TRPC implementation to match official example

--- app/routes/_app/users.tsx
+++ app/routes/_app/users.tsx
@@ -1,45 +1,43 @@
-import { useQuery } from "@tanstack/react-query";
+import { useSuspenseQuery } from "@tanstack/react-query";
 import { createFileRoute } from "@tanstack/react-router";
 
-import { Spinner } from "~/components/spinner";
-import { trpc } from "~/router";
+import { useTRPC } from "~/trpc/react";
 
 export const Route = createFileRoute("/_app/users")({
-  loader: async ({ context: { queryClient, trpc } }) =>
-    await queryClient.ensureQueryData(trpc.user.getAll.queryOptions()),
+  loader: async ({ context }) =>
+    await context.queryClient.prefetchQuery(
+      context.trpc.user.getAll.queryOptions(),
+    ),
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  const { data, isLoading } = useQuery(trpc.user.getAll.queryOptions());
+  const trpc = useTRPC();
+  const { data } = useSuspenseQuery(trpc.user.getAll.queryOptions());
 
   return (
     <article className="space-y-4">
       <h1 className="font-extrabold text-2xl">Users</h1>
       <section>
-        {isLoading ? (
-          <Spinner />
-        ) : data ? (
-          <table>
-            <thead>
-              <tr>
-                <th className="text-left">Email</th>
-                <th className="text-left">Name</th>
+        <table>
+          <thead>
+            <tr>
+              <th className="text-left">Email</th>
+              <th className="text-left">Name</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map(({ id, email, image, name }) => (
+              <tr key={id}>
+                <td>
+                  {image ? <img src={image} alt={email} /> : null}
+                  {email}
+                </td>
+                <td>{name}</td>
               </tr>
-            </thead>
-            <tbody>
-              {data.map(({ id, email, image, name }) => (
-                <tr key={id}>
-                  <td>
-                    {image ? <img src={image} alt={email} /> : null}
-                    {email}
-                  </td>
-                  <td>{name}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        ) : null}
+            ))}
+          </tbody>
+        </table>
       </section>
     </article>
   );
